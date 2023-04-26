7b5a03fae66b60af71fe6ec799f12ed50f3a5338
António Quaresma <antqua@outlook.pt>
2023-04-26 19:00:43 +0100

    displayBooks func to render book cards

--- index.js
+++ index.js
@@ -32,3 +32,61 @@ function addBookToLibrary() {
   // Add the new Book object to the myLibrary array
   myLibrary.push(newBook);
 }
+
+//Render the book cards on the Library
+function displayBooks() {
+    const libraryContainer = document.getElementById('book-cards');
+  
+    libraryContainer.innerHTML = '';
+  
+    myLibrary.forEach((book, index) => {
+      const bookCard = document.createElement('div');
+      bookCard.classList.add('book-card');
+  
+      const title = document.createElement('h2');
+      title.textContent = book.title;
+      bookCard.appendChild(title);
+  
+      const author = document.createElement('h3');
+      author.textContent = `By: ${book.author}`;
+      bookCard.appendChild(author);
+  
+      const pages = document.createElement('p');
+      pages.textContent = `${book.pages} pages`;
+      bookCard.appendChild(pages);
+  
+      const language = document.createElement('p');
+      language.textContent = `Language: ${book.language}`;
+      bookCard.appendChild(language);
+  
+      const read = document.createElement('p');
+      read.textContent = book.read ? 'Read' : 'Unread';
+      bookCard.appendChild(read);
+  
+      const readButton = document.createElement('button');
+      readButton.classList.add('book-read-btn');
+      readButton.textContent = book.read ? 'Mark as unread' : 'Mark as read';
+      readButton.addEventListener('click', () => {
+        book.toggleReadStatus();
+        read.textContent = book.read ? 'Read' : 'Unread';
+        readButton.textContent = book.read ? 'Mark as unread' : 'Mark as read';
+  
+        updateCounters();
+      });
+      bookCard.appendChild(readButton);
+  
+      const removeButton = document.createElement('button');
+      removeButton.classList.add('book-remove-btn');
+      removeButton.textContent = 'Remove';
+      removeButton.setAttribute('data-index', index);
+      removeButton.addEventListener('click', removeBookFromLibrary);
+      bookCard.appendChild(removeButton);
+  
+      libraryContainer.appendChild(bookCard);
+    });
+  
+    updateCounters();
+  }
+
+// Call the displayBooks function initially to display the book cards on the page
+displayBooks();
